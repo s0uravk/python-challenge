80d1af4d25d09b576f789c2563b4fbf5ea2849b3
Sourav Kumar <144293972+s0uravk@users.noreply.github.com>
2023-11-10 12:34:35 -0500

    Update main.py
    Main script updated

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -7,41 +7,71 @@ def vote_count():
         return count
 
 #input file path
-file_in = "PyPoll\Resources\election_data.csv"
+file_in = "election_data.csv"
 
-#declaring a dictionary variable
-candidates = {}
-votes = []
-count = 0
+
+candidatesNvotes = {}  #declaring a dictionary variable
+total_votes = 0        #to count total iterations hence total votes
+candidates_list = []   # to store keys of dictionary in a list
+votes_per_candidate = []   # to store values of dictionary in a list
+vote_percent = []      #to store vote percent calculated
+
 #open input file to be processed
 with open(file_in, 'r') as input_data :
     read = csv.reader(input_data, delimiter = ",")
     header = next(read)
     
-    for rows in read :
-        if rows[1] in candidates.keys(): 
-            if not rows[2] in candidates[rows[1]]:
-                candidates[rows[1]].append(rows[2])
-               
-        else: 
-            candidates[rows[1]] = [rows[2]]
-        
-        for people in candidates[rows[1]]:
-            if((candidates[rows[1]][0]) in rows[2]):
-                count += 1
-
-    votes.append(count)
-    print(votes)
-
-
+    #loopin through the data in csv file
+    for row in read :
+        total_votes += 1      #total votes count
+        if not row[2] in candidatesNvotes.keys(): #checking for candidate's name not in dictionary 
+            candidatesNvotes[row[2]] =1           #first iteration of a key is not present in the dictionary, hence it adds a key as candidates_
+                                                  #name and assign value for that first iteration
             
-                      
-            
-    print(candidates[rows[1]][2])   
-    print(candidates)
+        else:                                     #checking for candidate's name in dictionary 
+            candidatesNvotes[row[2]] += 1         #if name is found, adds one to the value of that key
    
+    candidates_list.append(list(candidatesNvotes.keys()))     #appends candidate names into a list
+    votes_per_candidate.append(list(candidatesNvotes.values())) ##appends votes per candidate into a list
     
+    #print results to the terminal
+    print("Election Results : ")
+    print("---------------------------------------------------------------------------------------------------")
+    print(f"Total Votes : {total_votes}")
+    print("---------------------------------------------------------------------------------------------------")
+    for votes in candidatesNvotes.values() :                #to print the candidate name, percentage of votes and number of votes a candidate won for each candidate
     
+        percent = votes/total_votes                  
+        vote_percent.append(percent)
+        candidate = candidates_list[0][vote_percent.index(percent)]
+        votes_won = votes_per_candidate[0][vote_percent.index(percent)]
+        
+        print(f"{candidate} : {percent :.2%} ({votes_won})")
+        
+    win_votes = max(votes_per_candidate[0])
+    winner = candidates_list[0][vote_percent.index(max(vote_percent))]
+    print("---------------------------------------------------------------------------------------------------")
+    print(f"Winner is {winner} : {vote_percent[0] :.2%} ")
+    
+#creating an output file path  
+outFile = "results.txt"
 
-
-
+#manipulation of result
+with open(outFile,'w') as output_data :
+    writer = csv.writer(output_data)
+    writer.writerow(["Election Results : "])
+    writer.writerow(["---------------------------------------------------------------------------------------------------"])
+    writer.writerow([f"Total Votes : {total_votes}"])
+    writer.writerow(["---------------------------------------------------------------------------------------------------"])
+    for votes in candidatesNvotes.values() :
+    
+        percent = int(votes)/total_votes
+        vote_percent.append(percent)
+        candidate = candidates_list[0][vote_percent.index(percent)]
+        votes_won = votes_per_candidate[0][vote_percent.index(percent)]
+        
+        writer.writerow([f"{candidate} : {percent :.2%} ({votes_won})"])
+    
+    writer.writerow(["---------------------------------------------------------------------------------------------------"])
+    writer.writerow([f"Winner is {winner} : {vote_percent[0] :.2%} "])
+    writer.writerow(["---------------------------------------------------------------------------------------------------"])
